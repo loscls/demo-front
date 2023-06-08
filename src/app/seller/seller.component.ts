b56819df18451895f4b829b08febf1bb8b7be8ea
loscls <lorenzo.j.scalise@gmail.com>
2023-06-08 21:02:28 +0200

    add(nav,seller,login,cart,home)

---
+++ src/app/seller/seller.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller',
+  templateUrl: './seller.component.html',
+  styleUrls: ['./seller.component.css']
+})
+export class SellerComponent {
+
+  signup(data: object): void{
+    console.log(data);
+  }
+
+}
